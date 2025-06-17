6f3741eeb30576632f15a39176c7332b971be76a
clagosu <clagosu@users.noreply.github.com>
2025-06-17 15:59:03 -0400

    create

---
+++ main.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "bucket_data" {
+  bucket = "mi-bucket-data"
+  acl    = "public-read"
+}
+
+resource "aws_security_group" "sg_inseguro" {
+  name        = "sg_inseguro"
+  description = "Grupo de seguridad"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
