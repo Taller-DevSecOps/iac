a77886212bf07a0e304f581b1e6e184b4b3ba419
clagosu <clagosu@users.noreply.github.com>
2025-06-17 17:14:28 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_s3_bucket" "bucket_data" {
   bucket = "mi-bucket-data"
-  acl    = "public-read"
+  acl    = "private"
 }
 
 resource "aws_security_group" "sg_inseguro" {
