f7304c0e94660e6499579dfefe69780febb6d27b
clagosu <clagosu@users.noreply.github.com>
2025-10-20 17:04:41 -0300

    allow public SSH access

--- main.tf
+++ main.tf
@@ -7,12 +7,6 @@ resource "aws_s3_bucket" "bucket_data" {
   acl    = "public-read"
 }
 
-resource "aws_s3_bucket_public_access_block" "access_good_1" {
-   bucket = aws_s3_bucket.bucket_data.id
-   block_public_acls   = true
-   block_public_policy = true
- }
-
 resource "aws_security_group" "sg_inseguro" {
   name        = "sg_inseguro"
   description = "Grupo de seguridad"
@@ -20,7 +14,7 @@ resource "aws_security_group" "sg_inseguro" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.1/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
     from_port   = 0
