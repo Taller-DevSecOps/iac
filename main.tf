506a47564f60938aaef644e8674c88f4a32853e0
clagosu <clagosu@users.noreply.github.com>
2026-05-12 15:06:28 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,16 +9,15 @@ provider "aws" {
   secret_key = "bar"
 }
 
-resource "aws_s3_bucket" "bucket_data" {
-  bucket = "mi-bucket-data"
-  acl    = "public-read"
-}
+resource "aws_instance" "example" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.sg_inseguro.id]
 
-resource "aws_s3_bucket_public_access_block" "access_good_1" {
-   bucket = aws_s3_bucket.bucket_data.id
-   block_public_acls   = true
-   block_public_policy = true
- }
+  tags = {
+    Name = "HelloWorld"
+  }
+}
 
 resource "aws_security_group" "sg_inseguro" {
   name        = "sg_inseguro"
@@ -27,7 +26,7 @@ resource "aws_security_group" "sg_inseguro" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.1/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
     from_port   = 0
