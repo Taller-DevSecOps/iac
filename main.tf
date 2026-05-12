09a119fc000f18b27975f9429e06094e892753cd
clagosu <clagosu@users.noreply.github.com>
2026-05-12 15:35:01 -0400

    Update AMI reference for AWS instance

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = data.aws_ami.ubuntu.id
+  ami           = "resolve:ssm:/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
   instance_type = "t3.micro"
   vpc_security_group_ids = [aws_security_group.sg_inseguro.id]
 
