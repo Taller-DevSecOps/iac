d3c8d15d49baf127875420c97d40e5079ca6a923
clagosu <clagosu@users.noreply.github.com>
2026-05-12 14:47:54 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,12 @@
 provider "aws" {
   region = "us-east-1"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_region_validation      = true
+  skip_requesting_account_id  = true
+
+  access_key = "foo"
+  secret_key = "bar"
 }
 
 resource "aws_s3_bucket" "bucket_data" {
