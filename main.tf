3406a7021f25796035e97940a6684e0989bdc3c1
clagosu <clagosu@users.noreply.github.com>
2026-08-03 18:51:06 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "aws_security_group" "sg_inseguro" {
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = "tcp"
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/16"]
   }
 }
